51c411850a09f3f2c259fd7a152b59c5911b0644
Willy <stedyyulius!gmail.com>
2017-10-07 23:19:13 +0700

    100% Done and Worked

---
+++ horus-vue/src/components/Hello.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="body">
+    <div class="login-form">
+      <div class="form-group logo">
+          <img width="150px" src="../assets/logo.png">
+      </div>
+       <div class="input form-group">
+         <input type="text" class="form-control" placeholder="Username " id="UserName" />
+         <i class="fa fa-user"></i>
+       </div>
+       <div class="form-group log-status">
+         <input type="password" class="form-control" placeholder="Password" id="Passwod" />
+         <i class="fa fa-lock"></i>
+       </div>
+       <select>
+         <option disabled selected>Select Origin</option>
+         <option>Jakarta</option>
+         <option>Bandung</option>
+         <option>Surabaya</option>
+         <option>Makasar</option>
+         <option>Semarang</option>
+         <option>Manado</option>
+         <option>Medan</option>
+         <option>Yogyakarta</option>
+       </select>
+       <select>
+         <option disabled selected>Select Destination</option>
+         <option>Singapore</option>
+         <option>Taiwan</option>
+         <option>Malaysia</option>
+         <option>Thailand</option>
+         <option>Vietnam</option>
+         <option>Philipines</option>
+         <option>China</option>
+         <option>Japan</option>
+       </select>
+       <br />
+       <br />
+       Departure Date
+       <input type="date" name="dateofbirth" id="dateofbirth">
+       Arrival Date
+       <input type="date" name="dateofbirth" id="dateofbirth">
+       <br />
+       <br />
+       <button class="loginBtn loginBtn--facebook" @click="login()">
+         Login with Facebook
+       </button>
+       <button type="button" class="log-btn" @click="login()">Log in</button>
+     </div>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    login(){
+        //  this.$router.push('/')
+        window.location = 'http://localhost:8081/vr/'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+[type="date"] {
+  background:#fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)  97% 50% no-repeat ;
+}
+[type="date"]::-webkit-inner-spin-button {
+  display: none;
+}
+[type="date"]::-webkit-calendar-picker-indicator {
+  opacity: 0;
+}
+
+input {
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  background-color: #fff;
+  padding: 3px 5px;
+  box-shadow: inset 0 3px 6px rgba(0,0,0,0.1);
+  width: 190px;
+}
+.loginBtn {
+  box-sizing: border-box;
+  position: relative;
+  /* width: 13em;  - apply for fixed size */
+  margin: 0.2em;
+  padding: 0 15px 0 46px;
+  border: none;
+  text-align: center;
+  line-height: 34px;
+  white-space: nowrap;
+  border-radius: 0.2em;
+  font-size: 16px;
+  color: #FFF;
+}
+.loginBtn:before {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 34px;
+  height: 100%;
+}
+.loginBtn:focus {
+  outline: none;
+}
+.loginBtn:active {
+  box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
+}
+/* Facebook */
+.loginBtn--facebook {
+  background-color: #4C69BA;
+  background-image: linear-gradient(#4C69BA, #3B55A0);
+  /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+  text-shadow: 0 -1px 0 #354C8C;
+}
+.loginBtn--facebook:before {
+  border-right: #364e92 1px solid;
+  background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
+}
+.loginBtn--facebook:hover,
+.loginBtn--facebook:focus {
+  background-color: #5B7BD5;
+  background-image: linear-gradient(#5B7BD5, #4864B1);
+}
+/* Google */
+.loginBtn--google {
+  /*font-family: "Roboto", Roboto, arial, sans-serif;*/
+  background: #DD4B39;
+}
+.loginBtn--google:before {
+  border-right: #BB3F30 1px solid;
+  background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
+}
+.loginBtn--google:hover,
+.loginBtn--google:focus {
+  background: #E74B37;
+}
+@import url(https://fonts.googleapis.com/css?family=Vibur);
+* {
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  font-family: arial;
+}
+.input{
+  margin-top: 50px;
+}
+.logo {
+  width: 120px;
+  height: 120px;
+  margin: auto;
+  display: block;
+}
+.logo-text{
+  width: 90px;
+  height: 45px;
+  margin: auto;
+  margin-bottom: 30px;
+  display: block;
+}
+body{
+  background-image: url("https://i.ytimg.com/vi/2yKsDZrEF7U/maxresdefault.jpg");
+  background-size: cover;
+}
+.login-form {
+  width: 350px;
+  padding: 20px 20px;
+  background: #eee;
+  -moz-border-radius: 4px;
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 100px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  -moz-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+.form-group {
+  position: relative;
+  margin-bottom: 15px;
+}
+.form-control {
+  width: 100%;
+  height: 50px;
+  border: none;
+  padding: 5px 7px 5px 15px;
+  background: #fff;
+  color: #666;
+  border: 2px solid #ddd;
+  -moz-border-radius: 4px;
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+}
+.form-control:focus, .form-control:focus + .fa {
+  border-color: #00AEEF;
+}
+.form-group .fa {
+  position: absolute;
+  right: 15px;
+  top: 17px;
+  color: #999;
+}
+.log-status.wrong-entry {
+  -moz-animation: wrong-log 0.3s;
+  -webkit-animation: wrong-log 0.3s;
+  animation: wrong-log 0.3s;
+}
+.log-status.wrong-entry .form-control, .wrong-entry .form-control + .fa {
+  border-color: #ed1c24;
+  color: #ed1c24;
+}
+.log-btn {
+  background: #00AEEF;
+  dispaly: inline-block;
+  width: 100%;
+  font-size: 16px;
+  height: 50px;
+  color: #fff;
+  text-decoration: none;
+  border: none;
+  -moz-border-radius: 4px;
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+}
+.link {
+  text-decoration: none;
+  color: #C6C6C6;
+  float: right;
+  font-size: 12px;
+  margin-bottom: 15px;
+}
+.link:hover {
+  text-decoration: underline;
+  color: #8C918F;
+}
+.alert {
+  display: none;
+  font-size: 12px;
+  color: #f00;
+  float: left;
+}
+@-moz-keyframes wrong-log {
+  0%, 100% {
+    left: 0px;
+  }
+  20% , 60% {
+    left: 15px;
+  }
+  40% , 80% {
+    left: -15px;
+  }
+}
+@-webkit-keyframes wrong-log {
+  0%, 100% {
+    left: 0px;
+  }
+  20% , 60% {
+    left: 15px;
+  }
+  40% , 80% {
+    left: -15px;
+  }
+}
+@keyframes wrong-log {
+  0%, 100% {
+    left: 0px;
+  }
+  20% , 60% {
+    left: 15px;
+  }
+  40% , 80% {
+    left: -15px;
+  }
+}
+</style>

1f6d1a2f702a32355824a7f4fa25091c2291b082
Willy <stedyyulius!gmail.com>
2017-10-08 07:10:12 +0700

    add invoice email

---
+++ server/routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+require('dotenv').config()
+var transport = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    auth: {
+        user: 'jago3333steve@gmail.com',
+        pass: '123gwkeren'
+    }
+}));
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/sendEmail', function(req, res, next) {
+    var job = {
+    from:`Horus <horus@gmail.com>`,
+    to: `stedy.yulius@orori.com`,
+    subject: `Horus Invoice`,
+    text: 'Done',
+    html: `<img src=${req.body.image} height="200" width="200">`
+  }
+
+  transport.sendMail(job, (error, info) => {
+          if (error) {
+            console.log(error);
+            res.send(error);
+          }
+          else{
+            console.log(`sukses`);
+            res.send('success!')
+          }
+      });
+
+});
+
+const accept = () =>{
+  console.log(`halo`);
+}
+
+
+module.exports = router;
